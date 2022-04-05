51ee35f08dfb95f2051ddae8ec474bab8067131d
Jotas <jproodian721@gmail.com>
2022-04-05 12:47:48 -0500

    set up initial route folder and file system

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,10 @@
+// import express router to be able to modularize the routes
+const router = require('express').Router();
+
+// const whateverTypeOfRoutesIUseHere = require('../apiRoutes/aforementionedroutesfilein the same directory)
+// another if needed
+
+// router.use(consts from above)
+// another if needed
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+// import express.js
 const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+// instantiate express server and assign it to app variable so that we can chain on other methods to the express server later
+const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// use middleware to parse strings, arrays, json objects
+app.use(express.json());
+app.use(express.urlencoded());
+// serve static files in public directory
+app.use(express.static('public'));
+
+// Use api routes
+app.use('/api', apiRoutes);
+// html routes
+app.use('/', htmlRoutes);
+
+// adding listen method to make server listen
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
+})
