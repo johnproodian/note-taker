943a9f3470e045221ff3161c43aa2fd81bf2a1c2
Jotas <jproodian721@gmail.com>
2022-04-05 15:02:47 -0500

    set up post route to create a new note

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,9 +1,25 @@
+const fs = require('fs');
+const path = require('path');
 const router = require('express').Router();
 const { notes } = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
-    console.log(notes);
     res.json(notes);
-})
+});
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    
+    notes.push(req.body);
+    fs.writeFileSync(
+        path.join(__dirname, "../../db/db.json"),
+        JSON.stringify({ notes }, null, 2)
+    );
+    res.json(notes);
+});
+
+// router.delete('/notes/:id', (req, res) => {
+
+// })
 
 module.exports = router;
